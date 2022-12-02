64b00bc62f850a71aafc98dc861fe50a6d5093b3
Emaprosk <emamagurno@hotmail.com>
2022-12-02 09:18:34 -0300

    Cambios en Login

--- Views/header.php
+++ Views/header.php
@@ -4,8 +4,11 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+     
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH?>styleGeneral.css">
+    <link rel="stylesheet" href="<?php echo CSS_PATH?>login.css">
 
      <title>LABIV</title>
 </head>
-<body>
+<body >
--- Views/index.php
+++ Views/index.php
@@ -1,24 +1,58 @@
-<main class="d-flex align-items-center justify-content-center height-100" >
-     <div class="content">
-          <header class="text-center">
-               <h2>INICIO</h2>
-          </header>
+<a href="" class="logo" target="_blank">  <?php // Logo de la empresa ?>
+  <img src="" alt="">
+</a>
 
-          <form action="" method="post" class="login-form bg-dark-alpha p-5 bg-light">
-               <div class="form-group">
-                    <label for="">Email</label>
-                    <input name="email-field" type="text" class="form-control form-control-lg" placeholder="Ingresar usuario" required>
-               </div>
-               <div class="form-group">
-                    <label for="">Contraseña</label>
-                    <input name="password-field" type="password" class="form-control form-control-lg" placeholder="Ingresar constraseña" required>
-               </div>
-               <button class="btn btn-primary btn-block btn-lg" type="submit">Iniciar Sesión</button>
-               <a href=""> Registrarse</a>
-               <br>
-               <br>
-               <a href="">Trabaja con nosotros...</a>
-          </form>
-     </div>
-</main>
-
+<div class="section">
+  <div class="container">
+    <div class="row full-height justify-content-center">
+      <div class="col-12 text-center align-self-center py-5">
+        <div class="section pb-5 pt-5 pt-sm-2 text-center">
+          <h6 class="mb-0 pb-3"><span>Log In </span><span>Sign Up</span></h6>
+          <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+          <label for="reg-log"></label>
+          <div class="card-3d-wrap mx-auto">
+            <div class="card-3d-wrapper">
+              <div class="card-front">
+                <div class="center-wrap">
+                  <div class="section text-center">
+                    <h4 class="mb-4 pb-3">Log In</h4>
+                    <div class="form-group">
+                      <input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off" required>
+                      <i class="input-icon uil uil-at"></i>
+                    </div>
+                    <div class="form-group mt-2">
+                      <input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off" required>
+                      <i class="input-icon uil uil-lock-alt"></i>
+                    </div>
+                    <a href="#" class="btn mt-4">submit</a>
+                    <p class="mb-0 mt-4 text-center"><a href="#0" class="link">Forgot your password?</a></p>
+                  </div>
+                </div>
+              </div>
+              <div class="card-back">
+                <div class="center-wrap">
+                  <div class="section text-center">
+                    <h4 class="mb-4 pb-3">Sign Up</h4>
+                    <div class="form-group">
+                      <input type="text" name="logname" class="form-style" placeholder="Your Full Name" id="logname" autocomplete="off" required>
+                      <i class="input-icon uil uil-user"></i>
+                    </div>
+                    <div class="form-group mt-2">
+                      <input type="email" name="logemail" class="form-style" placeholder="Your Email" id="logemail" autocomplete="off" required>
+                      <i class="input-icon uil uil-at"></i>
+                    </div>
+                    <div class="form-group mt-2">
+                      <input type="password" name="logpass" class="form-style" placeholder="Your Password" id="logpass" autocomplete="off" required>
+                      <i class="input-icon uil uil-lock-alt"></i>
+                    </div>
+                    <a href="#" class="btn mt-4">submit</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
