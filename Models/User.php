f3ad13cb79d8d85a349beeafe52311ac4c90b0e8
JereDoumic <jere.dou@hotmail.com>
2022-12-01 23:27:23 -0300

    Getters and Setters

--- Models/User.php
+++ Models/User.php
@@ -2,7 +2,44 @@
     namespace Models;
 
     class User{
+        private $id;
         private $email;
         private $password;
+
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        public function getEmail()
+        {
+                return $this->email;
+        }
+
+        public function setEmail($email)
+        {
+                $this->email = $email;
+
+                return $this;
+        }
+
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
+        }
     }
 ?>
